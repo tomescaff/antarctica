dde6b2fa8a7b44a8105227200f7f5d26ee1236bd
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-03-19 12:47:21 -0300

    refactoring visualization example

--- displaying/examples/ex_time_series.py
+++ displaying/examples/ex_time_series.py
@@ -1,22 +1,39 @@
 import sys
-
-sys.path.append('../../processing/')
-from processing.aws import AWS, AWSReader
-
-filepath = '../../data/wisc_aws_q10_2021_12/jas202112q10.txt'
-#filepath = '../../data/wisc_aws_q10_2021_12/kth202112q10.txt'
-
-aws_reader = AWSReader()
-aws = aws_reader.read_aws(filepath)
-
-temp = aws.real_time_series
-
-temp_sel = temp.sel(time=slice('2021-12-03 00:00:00', '2021-12-04 20:00:00'))
-
+from datetime import datetime
 import matplotlib.pyplot as plt
 
+sys.path.append('../../processing/')
+from processing.aws import AWS, AWSWiscReader
+
+# select the filepath with data from wisc aws
+filepath = '../../data/wisc_aws_q10_2021_12/jas202112q10.txt'
+#filepath = '../../data/wisc_aws_q10_2021_12/kth202112q10.txt'
+#filepath = '../../data/wisc_aws_q10_2021_12/byd202112q10.txt'
+#filepath = '../../data/wisc_aws_q10_2021_12/lda202112q10.txt'
+
+# read the file to aws station object
+aws_reader = AWSWiscReader()
+aws = aws_reader.read_aws(filepath)
+
+# get the original time series
+temp = aws.real_time_series
+
+# select period near eclipse
+temp_sel = temp.sel(time=slice('2021-12-03 00:00:00', '2021-12-04 20:00:00'))
+
+# define the eclipse timestamps for jas station
+time_pre_eclipse = datetime(2021, 12, 4, 6, 35, 52) # 06:35:52.8
+time_eclipse = datetime(2021, 12, 4, 7, 29, 40) # 07:29:40
+time_pos_eclipse = datetime(2021, 12, 4, 8, 24, 46, 2) # 08:24:46.2
+
+# plot the data
 fig = plt.figure(figsize=(12,6))
-plt.plot(temp_sel.time, temp_sel.values, lw=0.8, c='b')
+# red rectangle during eclipse
+plt.gca().axvspan(time_pre_eclipse, time_pos_eclipse, alpha=0.2, color='red')
+# red vertical line in eclipse 
+plt.axvline(time_eclipse, lw=0.8, c='r')
+# plot original temperature time series
+plt.plot(temp_sel.time, temp_sel.values, lw=0.8, c='k')
 plt.xlabel('Time (UTC)')
 plt.ylabel('Temperature (deg C)')
 plt.grid(ls='--', lw='0.2', c='grey')
